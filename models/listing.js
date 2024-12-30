071b842fc57c3a3e35a13ae2f16b476a0234dba6
shhriiii <149945420+shhriiii@users.noreply.github.com>
2024-12-30 20:47:28 +0530

    added review section html code without backend connection in show.ejs

--- models/listing.js
+++ models/listing.js
@@ -13,9 +13,16 @@ const listingSchema = new Schema({
         default: "https://images.unsplash.com/photo-1530631673369-bc20fdb32288?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set: (v) => v ==="" ? "https://images.unsplash.com/photo-1530631673369-bc20fdb32288?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
     },
+
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review"
+        },
+    ],
 })
 
 //export
