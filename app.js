c4552f7bd244447278c467df688d74153d6a24bf
shhriiii <149945420+shhriiii@users.noreply.github.com>
2024-12-30 21:15:14 +0530

    added review route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 
@@ -121,6 +122,21 @@ app.delete("/listings/:id" , wrapAsync(async(req , res ) => {
     res.redirect("/listings")
 
 }));
+
+//review route 
+//post route
+app.post("/listings/:id/reviews" , wrapAsync(async(req , res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    // console.log("new review saved");
+    // res.send("new review saved");
+    res.redirect(`/listings/${listing._id}`);
+
+}));
+
 
 
 //if called route deosnt exist
