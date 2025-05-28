508b1b65dd6367a690e79b140a8226b5efb9cc8b
shhriiii <149945420+shhriiii@users.noreply.github.com>
2025-05-29 00:32:21 +0530

    added mongoAtlas

--- app.js
+++ app.js
@@ -19,19 +19,21 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo"); //for storing session in mongo db
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust"
+// const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust"
+const dbUrl = process.env.ATLASDB_URL;
 main()
 .then(()=>{
     console.log("cnntn succesfull");
 })
 .catch((err) => console.log(err));
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 
   }
   app.set("view engine" , "ejs");
@@ -46,8 +48,23 @@ async function main() {
   app.use(express.static(path.join(__dirname, "/public")));
 
 
+  const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+      secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+  });
+  
+  store.on("error", () => {
+    console.log("ERROR in MONGO SESSION STORE");
+  });
+  
+
+
   const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
