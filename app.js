8da4910a019254316cea79064da38b6e4f540f9e
shhriiii <149945420+shhriiii@users.noreply.github.com>
2025-05-30 01:37:03 +0530

    added new css

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 // const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust"
-const dbUrl = process.env.ATLASDB_URL;
+  const dbUrl = process.env.ATLASDB_URL;
 main()
 .then(()=>{
     console.log("cnntn succesfull");
--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ module.exports.signup = async (req, res) => {
         if(err){
             return next(err);
         }
-        req.flash("success", "Welcome to Wanderlust!");
+        req.flash("success", "Welcome to Stay Space!");
     res.redirect("/listings");
     });
     
@@ -34,7 +34,7 @@ module.exports.renderLoginForm = (req,res) => {
 };
 
 module.exports.login =  async (req, res) => {
-    req.flash("success", "Welcome to Wanderlust! You are logged in!");
+    req.flash("success", "Welcome to Stay Space! You are logged in!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
     
